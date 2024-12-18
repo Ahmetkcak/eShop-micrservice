0ca5999e0f53bf86958f39f46bf06a9401a5f3b0
ahmetkcak <ahmet.aliberk07@gmail.com>
2024-12-18 22:56:26 +0300

    test(order-service): added integration tests for order creation

--- order-service/src/test/java/com/microservice/orderservice/OrderServiceApplicationTests.java
+++ order-service/src/test/java/com/microservice/orderservice/OrderServiceApplicationTests.java
@@ -1,15 +1,61 @@
 package com.microservice.orderservice;
 
-import org.junit.jupiter.api.Test;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+
+import org.hamcrest.Matchers;
+import org.junit.Test;
+import org.junit.jupiter.api.BeforeEach;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
 import org.springframework.context.annotation.Import;
+import org.testcontainers.containers.MySQLContainer;
+
+import io.restassured.RestAssured;
 
 @Import(TestcontainersConfiguration.class)
-@SpringBootTest
-class OrderServiceApplicationTests {
+@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
+public class OrderServiceApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+	@ServiceConnection
+    static MySQLContainer<?> mySQLContainer = new MySQLContainer<>("mysql:latest");
+    @LocalServerPort
+    private Integer port;
 
+    @BeforeEach
+    void setup() {
+        RestAssured.baseURI = "http://localhost";
+        RestAssured.port = port;
+    }
+
+    static {
+        mySQLContainer.start();
+    }
+
+    @Test
+    public void createOrderSuccessfully_ShouldReturnOrderPlacedSuccessfully() {
+        String submitOrderJson = """
+                {
+                     "skuCode": "iphone_15",
+                     "price": 1000,
+                     "quantity": 1
+                }
+                """;
+
+
+        var responseBodyString = RestAssured.given()
+                .contentType("application/json")
+                .body(submitOrderJson)
+                .when()
+                .post("/api/orders")
+                .then()
+                .log().all()
+                .statusCode(201)
+                .extract()
+                .body().asString();
+
+        assertThat(responseBodyString, Matchers.is("Order Placed Successfully"));
+    }
 }
