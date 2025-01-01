a2c9e0453e2da243b14e62619f917f2e73ded609
ahmetkcak <ahmet.aliberk07@gmail.com>
2025-01-01 12:04:15 +0300

    feat(api-gateway): added observability support with Loki logging and Micrometer integration

--- order-service/src/main/java/com/microservice/orderservice/config/RestClientConfig.java
+++ order-service/src/main/java/com/microservice/orderservice/config/RestClientConfig.java
@@ -15,17 +15,23 @@ import org.springframework.web.service.invoker.HttpServiceProxyFactory;
 
 import com.microservice.orderservice.client.InventoryClient;
 
+import io.micrometer.observation.ObservationRegistry;
+import lombok.RequiredArgsConstructor;
+
 @Configuration
+@RequiredArgsConstructor
 public class RestClientConfig {
     
     @Value("${inventory.url}")
     private String inventoryServiceUrl;
+    private final ObservationRegistry observationRegistry;
 
     @Bean
     public InventoryClient inventoryClient() {
         RestClient restClient = RestClient.builder()
                 .baseUrl(inventoryServiceUrl)
                 .requestFactory(getClientRequestFactory())
+                .observationRegistry(observationRegistry)
                 .build();
         var restClientAdapter = RestClientAdapter.create(restClient);
         var httpServiceProxyFactory = HttpServiceProxyFactory.builderFor(restClientAdapter).build();
